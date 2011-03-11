9188f4a2f673a8e8f3978c8f860aa7b22acbbefd
Alex.Helfet <Alex.Helfet@parkerfox.co.uk>
2011-03-11 10:49:23 +0000

    Fixed bug in Fetch-AllGitSvn

--- AutoLoad/Fetch-AllGitSvn.ps1
+++ AutoLoad/Fetch-AllGitSvn.ps1
@@ -4,7 +4,7 @@ function Fetch-AllGitSvn()
     $projectsToFetch = gci $gitProjectsRoot | ?{$_.psiscontainer -and $_.name -ne "LoanBookUK - broken"} 
     foreach($project in $projectsToFetch)
     {
-        $path = $_.fullname
+        $path = $project.fullname
         Write-Host "Fetching $path"
         cd $path
         git svn fetch
