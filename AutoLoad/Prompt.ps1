07f3e44cc27e09b389739a1f890afb5dfca8c186
Alex Helfet <alex.helfet@googlemail.com>
2011-03-17 04:12:14 +0000

    SVN prompt.

--- AutoLoad/Prompt.ps1
+++ AutoLoad/Prompt.ps1
@@ -1,16 +1,59 @@
 function prompt {
+    $host.ui.rawui.WindowTitle = "PS " + $(get-location)
+    
     Write-Host -foregroundcolor yellow -nonewline -object ($(
         if (test-path variable:/PSDebugContext) { '[DBG]: ' } else { '' }
     ) + 'PS ')
 
     Write-Host($pwd) -nonewline -foregroundcolor yellow
     
-    # Git Prompt
-    $Global:GitStatus = Get-GitStatus
-    Write-GitStatus $GitStatus
-     
-    
+    if      (Write-GitPrompt) {}
+    elseif  (Write-SvnPrompt) {}
+
     Write-Host ">" -foregroundcolor yellow -nonewline
     
     return " "
+}
+
+function Write-GitPrompt {
+    $Global:GitStatus = Get-GitStatus
+    
+    if($GitStatus -ne $null)
+    {
+        Write-GitStatus $GitStatus
+        return $true
+    }
+    
+    return $false
+}
+
+function Write-SvnPrompt {
+    if(!(Test-Path .svn)) {
+        return $false
+    }
+    
+    $other = 0
+    $added = 0
+    $modified = 0
+    
+    switch -regex (svn st) {
+       "^\?"    { $other    += 1 }
+       "^A"     { $added    += 1 }
+       "^M"     { $modified += 1 }
+       default  {}
+    }
+
+    Write-Host " [" -nonewline
+    Write-Host "SVN" -nonewline -fore cyan
+    
+    Write-Host " add:" -nonewline -fore yellow
+    Write-Host $added -nonewline -fore green
+    Write-Host " mod:" -nonewline -fore yellow
+    Write-Host $modified -nonewline -fore green
+    Write-Host " oth:" -nonewline -fore yellow
+    Write-Host $other -nonewline -fore green
+    
+    Write-Host "]" -nonewline
+
+    return $true
 }
