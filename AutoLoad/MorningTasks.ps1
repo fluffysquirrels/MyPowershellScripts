c59dd1efa53de2dfe67f57b542cea9cd0cd909f2
Alex.Helfet <Alex.Helfet@parkerfox.co.uk>
2011-04-14 11:30:52 +0100

    Run-MorningTasks

---
+++ AutoLoad/MorningTasks.ps1
@@ -0,0 +1,24 @@
+function Run-MorningTasks {
+    cd $profileDir
+    
+    Write-Host "Pulling from WindowsPowerShell Git repository . . ."
+    git pull
+    . p;
+
+    Write-Host ""
+    Write-Host "*****"
+    
+    Write-Host "Pulling from Parker Fox SVN repository . . ."
+    Fetch-AllGitSvn
+
+    Write-Host ""
+    Write-Host "*****"
+    
+    if($env:COMPUTERNAME -eq "BUNGLE") {
+        Write-Host "Backing up BUNGLE . . ."
+        Backup-Bungle
+    }
+    else {
+        Write-Host "Can't run backup, not on BUNGLE!"
+    }
+}
