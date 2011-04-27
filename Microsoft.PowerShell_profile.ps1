b66ba2e3864ba100be07befc87584eeef2455504
Alex.Helfet <Alex.Helfet@parkerfox.co.uk>
2011-04-27 12:20:45 +0100

    Tidier load scripts prompt.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,12 +4,18 @@ $ErrorActionPreference = "Stop"
 
 $profileDir = split-path $profile
 
-function Load-AutoLoadScripts($path)
+function Load-AutoLoadScripts
+(
+    $path,
+    [switch] $verbose = $false
+)
 {
     if(!(test-path $path)) {
         Write-Host "Skipping missing auto-load directory $path" -fore Yellow
         return
     }
+    
+    Write-Host "Loading scripts from '$path' and sub-directories."
 
     $toLoad = @(gci -recurse $path | 
                ?{$_ -is "System.IO.FileInfo" -and
@@ -18,10 +24,14 @@ function Load-AutoLoadScripts($path)
                
     foreach($script in $toLoad)
     {
-        . Load-Script $script
+        . Load-Script $script -verbose:$verbose
     }
 }
-function Load-Script($scriptFile)
+function Load-Script
+(
+    $scriptFile,
+    [switch] $verbose = $false
+)
 {
     # If $scriptFile is a path string, fetch the fileinfo from that path.
     if ($scriptFile -is "String")
@@ -35,9 +45,11 @@ function Load-Script($scriptFile)
         }
     }
     
-    # $scriptFile is a fileinfo object.
-    write-host ("Loading {0}\" -f $scriptFile.Directory.FullName) -nonewline
-    write-host $scriptFile.Name -foregroundcolor yellow
+    if($verbose) {
+        # Note: $scriptFile is a fileinfo object.
+        write-host ("Loading {0}\" -f $scriptFile.Directory.FullName) -nonewline
+        write-host $scriptFile.Name -foregroundcolor yellow
+    }
     
     . $scriptFile.FullName
 }
@@ -46,43 +58,32 @@ function Load-ParkerFoxScripts()
 {
     if(Is-AtWork)
     {
-        . Load-Script "D:\Projects\SVN\DeployTools\Scripts\Main.ps1"
-        . Load-Script "D:\Projects\SVN\DeployTools\Scripts\For deployment server\Setup IIS lib.ps1"
-        . Load-Script "D:\Projects\SVN\AspNetStats\Scripts\Poller.ps1"
+        . Load-Script "D:\Projects\SVN\DeployTools\Scripts\Main.ps1" -verbose
+        . Load-Script "D:\Projects\SVN\DeployTools\Scripts\For deployment server\Setup - IIS lib.ps1" -verbose
+        . Load-Script "D:\Projects\SVN\AspNetStats\Scripts\Poller.ps1" -verbose
     }
     else
     {
-        . Load-Script "C:\Data\Code\Parker Fox\Git\DeployTools\Scripts\Main.ps1"
+        . Load-Script "C:\Data\Code\Parker Fox\Git\DeployTools\Scripts\Main.ps1" -verbose
     }
 }
 
 function Is-AtWork()
 {
-    $workPcName = "BUNGLE"
-    $thisPcName = gc env:computername
-    
-    $atWork = $thisPcName -eq $workPcName
-    
-    if(!$atWork)
-    {
-        Write-Host "Not loading Parker Fox scripts because we're on '$thisPcName', not '$workPcName'."
-    }
-    
-    return $atWork
+    return $env:computername -eq "BUNGLE"
 }
 
 function Load-Profile() {
 
     "`nLoading profile scripts . . . `n"
     . Load-AutoLoadScripts (join-path $profileDir "AutoLoad")
-    ""
-    Init-Ssh
-    ""
     Import-Module pscx
     Import-Module posh-git
     # Import-Module PowerTab
-    ""
+
     . Load-ParkerFoxScripts
+    ""
+    Init-Ssh
     ""
     " ************* "
     ""
