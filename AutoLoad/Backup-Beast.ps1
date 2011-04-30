cb0c7a0b0fbfd34c52ba5894ee096a7598af74da
Alex Helfet <alex.helfet@googlemail.com>
2011-04-30 11:30:52 +0100

    Beast backup finds backup drive all on its little own.

--- AutoLoad/Backup-Beast.ps1
+++ AutoLoad/Backup-Beast.ps1
@@ -1,6 +1,7 @@
 function Backup-Beast()
 {
-    $backupBase = "E:\Backup"
+    $backupDrive = Get-BeastBackupDrivePath
+    $backupBase = join-path $backupDrive "Backup"
     $logPath = join-path $backupBase "robo_log.txt"
     
     if(test-path $logPath)
@@ -16,4 +17,18 @@ function Backup-Beast()
     BackupDir "c:\Users\Alex\AppData\Roaming\Tomboy\notes"    (join-path $backupBase "Tomboy notes")
     BackupDir "c:\Users\Alex\Documents\WindowsPowerShell"     (join-path $backupBase "WindowsPowerShell")
     BackupDir "c:\Data"                                       (join-path $backupBase "Data")
+}
+function Get-BeastBackupDrivePath()
+{
+    $backupVolumeName = "Cigar Case"
+    
+    $volumes = @(gwmi win32_logicaldisk |
+        ?{$_.drivetype -eq 3 -and $_.VolumeName -eq $backupVolumeName} |
+        %{$_.deviceid})
+
+    if($volumes.Length -ne 1) {
+        throw "Couldn't find exactly one backup drive with volume name '$backupVolumeName'."
+    }
+    
+    return $volumes[0]
 }
