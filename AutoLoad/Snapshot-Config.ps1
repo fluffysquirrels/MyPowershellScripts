89e4dabddca331a6347b686acf2235af0f3c3d91
Alex.Helfet <Alex.Helfet@parkerfox.co.uk>
2011-06-27 17:16:09 +0100

    Snapshot-Config.

---
+++ AutoLoad/Snapshot-Config.ps1
@@ -0,0 +1,24 @@
+function Snapshot-Config
+(
+    $treeRootPath = ".",
+    $backupRootPath =  (Get-DefaultSnapshotConfigDir)
+)
+{
+    $treeRootPath = convert-path $treeRootPath
+    $files = @(gci -recurse $treeRootPath -filter *.config)
+    
+    foreach($file in $files) {
+        $relPath = $file.FullName.Substring($treeRootPath.length)
+        $targetPath = join-path $backupRootPath $relPath
+        mkdir -force (split-path $targetPath) | Out-Null
+        cp $file.fullname $targetPath
+    }
+}
+
+function Get-DefaultSnapshotConfigDir()
+{
+    $userDir = $env:userprofile
+    $machine = $env:COMPUTERNAME
+    $dateStr = [DateTime]::Now.ToString("yyyy-MM-dd_HH-mm")
+    return "$userDir\Documents\ConfigSnapshot_$($machine)_$dateStr"
+}
